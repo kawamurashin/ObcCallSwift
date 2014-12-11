18ff10e24666f0537dd0694e486acffb7f691d58
kawamurashin <kawamura@mztm.jp>
2014-12-11 18:37:14 +0900

    Initial Commit

---
+++ ObcCallSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObcCallSwift
+//
+//  Created by 川村森 on 2014/12/11.
+//  Copyright (c) 2014年 kawamura shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObcCallSwift/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObcCallSwift
+//
+//  Created by 川村森 on 2014/12/11.
+//  Copyright (c) 2014年 kawamura shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
